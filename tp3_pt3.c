a4edb2dabde0d2782cbd86891a6e012909bfcc91
Velaztiqui Trigo Mauro Nicolas <trigotex@gmail.com>
2024-04-16 16:45:39 -0300

    Punto 3

--- tp3_pt3.c
+++ tp3_pt3.c
@@ -1,9 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(){
 
-    
+    char** misNombres = malloc(sizeof(int *) * 5);
+    char *nombre;
+    for(int i = 0; i < 5; i++){        
+        nombre = malloc(sizeof(char) * 50);
+        printf("Ingrese su nombre: ");
+        gets(nombre);
+        nombre = (char*) realloc(nombre, sizeof(strlen(nombre)+ 1));
+        misNombres[i] = nombre;
+
+    }
+
+    for(int i = 0; i < 5; i++){
+
+        printf("\nNombre %d: %s", i + 1, misNombres[i]);
+
+    }
+
+    for(int i = 0; i < 5; i++){
+        free(misNombres[i]);
+    }
+    free(misNombres);
 
     return 0;
 }
